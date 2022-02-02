b8246e58b6cf41dc752d843c7d353b724fef2102
Juan Martin <er.blacky@gmail.com>
2022-02-03 00:49:10 +0100

    DNIApp: finalizado

--- apps/myapp/src/app/app.module.ts
+++ apps/myapp/src/app/app.module.ts
@@ -8,18 +8,12 @@ import { PresentacionComponent } from './presentacion/presentacion.component';
 import { DniComponent } from './componentes/dni/dni.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { ImcComponent } from './componentes/imc/imc.component';
-import { EntradaDatosComponent } from './componentes/imc/entrada-datos/entrada-datos.component';
-import { ResultadosComponent } from './componentes/imc/resultados/resultados.component';
 
 @NgModule({
   declarations: [ AppComponent,
                   NxWelcomeComponent,
                   PresentacionComponent,
-                  DniComponent,
-                  ImcComponent,
-                  EntradaDatosComponent,
-                  ResultadosComponent
+                  DniComponent
                 ],
   imports: [BrowserModule,
             FormsModule,
