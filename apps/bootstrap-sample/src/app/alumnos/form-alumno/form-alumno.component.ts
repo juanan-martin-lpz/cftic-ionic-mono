145388193c2a5f35b58d63f0855ad5abfed4ad33
Juan Antonio Martin Lopez <juanan.martin.lpz@gmail.com>
2022-02-18 13:02:56 +0100

    Bootstrap: Upload de imagenes - Sin terminar - Backup de fin de semana

--- apps/bootstrap-sample/src/app/alumnos/form-alumno/form-alumno.component.ts
+++ apps/bootstrap-sample/src/app/alumnos/form-alumno/form-alumno.component.ts
@@ -4,6 +4,8 @@ import { AlumnoService } from '../../servicios/alumno.service';
 import { IAlumno } from '../../models/ialumno';
 import { Alumno } from '../../models/alumno';
 
+import bsCustomFileInput from 'bs-custom-file-input';
+
 @Component({
   selector: 'angular-monorepo-form-alumno',
   templateUrl: './form-alumno.component.html',
@@ -13,14 +15,16 @@ export class FormAlumnoComponent implements OnInit {
 
   private id: number;
   public alumno: IAlumno;
-  private nuevo: boolean;
+
+  public imagen?: File;
+  public imagenTemp?: string;
 
   constructor(private router: Router, private route: ActivatedRoute, private alumnoService: AlumnoService) {
 
+    bsCustomFileInput.init();
+
     this.id = 0;
     this.alumno = new Alumno();
-
-    this.nuevo = true;
 
     const id = this.route.snapshot.paramMap.get('id') || 0;
 
@@ -28,12 +32,9 @@ export class FormAlumnoComponent implements OnInit {
       this.id = +id;
     }
 
-    if (this.id > 0) {
-      this.nuevo = false;
-      this.alumnoService.obtenerAlumnoPorId(this.id).subscribe(alumno => {
-        this.alumno = alumno;
-      });
-    }
+    this.alumnoService.obtenerAlumnoPorId(this.id).subscribe(alumno => {
+      this.alumno = alumno;
+    });
 
 
   }
@@ -44,35 +45,51 @@ export class FormAlumnoComponent implements OnInit {
 
   guardar() {
 
-    if (!this.nuevo) {
-      this.alumnoService.modificarAlumno(this.alumno).subscribe(
-        {
-          complete: () => {
-            this.router.navigateByUrl('/alumnos');
-          },
-          error: (e) => {
-            // Gestion del error
-          }
+    this.alumnoService.modificarAlumno(this.alumno).subscribe(
+      {
+        complete: () => {
+          this.router.navigateByUrl('/alumno');
+        },
+        error: (e) => {
+          // Gestion del error
         }
-      )
-    }
-    else {
-      this.alumnoService.nuevoAlumno(this.alumno).subscribe(
-        {
-          complete: () => {
-            this.router.navigateByUrl('/alumnos');
-          },
-          error: (e) => {
-            // Gestion del error
-          }
-        }
-      )
-    }
+      }
+    )
   }
 
   cancelar() {
     // Nos vamos
     this.router.navigateByUrl('/alumnos');
     //
+  }
+
+  seleccionImagen( event: Event ) {
+
+    console.log('seleccion Image');
+
+    // @ts-ignore: Object is possibly 'null'.
+    const archivo = event.target.files[0];
+
+    if ( !archivo ) {
+      // @ts-ignore: Object is possibly 'null'.
+      console.log('Archivo null');
+      //this.imagen = null;
+      return;
+    }
+
+    console.log('Archivo OK');
+
+    this.imagen = archivo;
+
+    const reader = new FileReader();
+
+    // @ts-ignore: Object is possibly 'null'.
+    reader.readAsDataURL( archivo );
+
+    console.log('Archivo en imageTemp');
+
+    // @ts-ignore: Object is possibly 'null'.
+    reader.onloadend = () => this.imagenTemp = reader.result.toString();
+
   }
 }
